83cc46e674c97480af2037f25ffe710dedbaad9c
jcfloresnaj <jcflores0288@unitec.edu>
2015-03-11 00:34:32 -0600

    Second commit

--- Console.pro
+++ Console.pro
@@ -13,8 +13,12 @@ TEMPLATE = app
 
 
 SOURCES += main.cpp\
-        mainwindow.cpp
+        mainwindow.cpp \
+    keyreturnevent.cpp \
+    commandhandler.cpp
 
-HEADERS  += mainwindow.h
+HEADERS  += mainwindow.h \
+    keyreturnevent.h \
+    commandhandler.h
 
 FORMS    += mainwindow.ui
--- keyreturnevent.h
+++ keyreturnevent.h
@@ -1,10 +1,51 @@
 #ifndef KEYRETURNEVENT_H
 #define KEYRETURNEVENT_H
+#include <QObject>
+#include <QKeyEvent>
+#include <QMainWindow>
+#include <ui_mainwindow.h>
+#include <QDebug>
+#include <QProcess>
+#include <QString>
+#include <QDir>
 
-class KeyReturnEvent
+class commandHandler;
+
+class KeyReturnEvent: public QObject
 {
+    Q_OBJECT
+    int stateProcess;
+    QTextCursor cursor;
+    int position;
+    int lastPos;
+    QMainWindow *Parent;
+    Ui_MainWindow* ParentUI;
+    QProcess* myProcess = NULL;
+    QDir *myDir;
+    QDir *Location;
+    commandHandler *handler;
 public:
-    KeyReturnEvent();
+    KeyReturnEvent(QMainWindow *Parent,Ui_MainWindow* ParentUI);
+    void prompt();
+    void prompt(QString);
+    QDir *getLocation() const;
+    void setLocation(QDir *value);
+
+    QDir *getMyDir() const;
+    void setMyDir(QDir *value);
+
+    QProcess *getMyProcess() const;
+    void setMyProcess(QProcess *value);
+
+protected:
+    bool eventFilter(QObject *obj, QEvent *event);
+
+public slots:
+    void processOutput();
+    void processOutputError();
+    void finishedProcess(int, QProcess::ExitStatus);
 };
+
+#include "commandhandler.h"
 
 #endif // KEYRETURNEVENT_H
